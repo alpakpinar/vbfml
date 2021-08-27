0b2025ef3314951fdcfad6656e6d211db65a3d7d
Andreas Albert <andreas.albert@cern.ch>
2021-08-27 12:47:10 +0200

    Add missing filge

---
+++ vbfml/models.py
@@ -0,0 +1,31 @@
+from keras.layers import Dense
+from keras.models import Sequential
+
+
+def sequential_dense_model(
+    n_features: int, n_layers: int, n_nodes: "list[int]", n_classes: int
+) -> Sequential:
+    model = Sequential()
+
+    assert (
+        len(n_nodes) == n_layers,
+        "Inconsistent number of layers and node specification!",
+    )
+
+    model.add(
+        Dense(
+            n_nodes[0],
+            input_dim=n_features,
+            activation="relu",
+        )
+    )
+    for ilayer in range(1, n_layers):
+        model.add(
+            Dense(
+                n_nodes[ilayer],
+                activation="relu",
+            )
+        )
+    model.add(Dense(n_classes, activation="softmax"))
+    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+    return model
