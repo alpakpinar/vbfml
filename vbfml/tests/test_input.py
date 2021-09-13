d623ad3c13e30171ad1d6f67b8880bcfe36e3adc
Andreas Albert <andreas.albert@cern.ch>
2021-09-13 16:44:54 +0200

    More testing

--- vbfml/tests/test_input.py
+++ vbfml/tests/test_input.py
@@ -1,8 +1,8 @@
+import os
 from unittest import TestCase
 
-from vbfml.training.input import (
-    dataset_from_file_name_bucoffea,
-)
+from vbfml.tests.util import make_tmp_dir, create_test_tree
+from vbfml.training.input import dataset_from_file_name_bucoffea, load_datasets_bucoffea
 
 
 class TestInput(TestCase):
@@ -26,3 +26,28 @@ class TestInput(TestCase):
                 dataset_from_file_name_bucoffea("/" + input_value),
                 expected_output_value,
             )
+
+
+class TestLoadDatasetsBucoffea(TestCase):
+    def setUp(self):
+        self.wdir = make_tmp_dir()
+        self.addCleanup(os.rmdir, self.wdir)
+
+        files = [
+            os.path.join(self.wdir, "tree_firstdataset_2017.root"),
+            os.path.join(self.wdir, "tree_seconddataset_2017.root"),
+        ]
+
+        for file in files:
+            create_test_tree(
+                filename=file,
+                treename="sr_vbf",
+                branches=["a", "b"],
+                n_events=10,
+                value=1,
+            )
+            self.addCleanup(os.remove, file)
+
+    def test_load_datasets_bucoffea(self):
+        datasets = load_datasets_bucoffea(self.wdir)
+        self.assertEqual(len(datasets), 2)
