f9c62b0ee8ddffc7ee0871fe610dc4f47682c6ba
Alp <aakpinar@bu.edu>
2022-03-07 20:03:35 +0100

    Model range argument for comparisons

--- vbfml/helpers/compare.py
+++ vbfml/helpers/compare.py
@@ -1,9 +1,10 @@
 import os
 
 from glob import glob
-from typing import Dict, List, Optional
+from typing import Dict, List, Tuple, Optional
 from dataclasses import dataclass, field
 from matplotlib import pyplot as plt
+from collections import OrderedDict
 
 from vbfml.training.data import TrainingLoader
 
@@ -18,13 +19,15 @@ class HistorySet:
     """
 
     output_directory: str
-    histories: "dict" = field(default_factory=dict)
+    histories: "dict" = field(default_factory=OrderedDict)
 
-    def accumulate_histories(self, num_models: int = -1) -> None:
+    def __len__(self):
+        """Returns the number of histories that this object holds."""
+        return len(self.histories)
+
+    def accumulate_histories(self) -> None:
         """Retrieve the set of model directories and accumulate the history data for all models."""
         dirs = glob(pjoin(self.output_directory, "model_v*"))
-        if num_models > 0 and num_models < len(dirs):
-            dirs = dirs[:num_models]
 
         for model_dir in dirs:
             loader = TrainingLoader(model_dir)
@@ -35,12 +38,23 @@ class HistorySet:
                 print(f"WARNING: history.pkl not found for {model_key}, skipping.")
                 continue
 
-    def plot(self, quantity: str = "loss"):
-        """For the set of models, plot the quantity."""
-        if len(self.histories) == 0:
+    def plot(
+        self, quantity: str = "loss", model_range: Tuple[int, int] = (0, -1)
+    ) -> None:
+        """
+        For the set of models, plot the training metric (quantity).
+        model_range argument can be specified if only a subset of models
+        are to be plotted on the same plot.
+        """
+        if len(self) == 0:
             raise RuntimeError(
                 "No histories found, did you forget to call accumulate_histories()?"
             )
+
+        model_range_start, model_range_end = model_range
+        if model_range_end > 0 and model_range_end < len(dirs):
+            histories = list(self.histories.items())[model_range_start:model_range_end]
+            histories = dict(histories)
 
         fig, ax = plt.subplots()
         for key, history in self.histories.items():
@@ -53,6 +67,7 @@ class HistorySet:
 
         if "loss" in quantity:
             ax.set_yscale("log")
+            ax.set_ylim(3e-5, 8e-5)
         elif "accuracy" in quantity:
             ax.set_ylim(0, 1.1)
 
