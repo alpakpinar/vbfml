618c4ad8a2780b87a74cdd1cff3e5a2def627ca6
Alp <aakpinar@bu.edu>
2022-05-16 20:15:38 +0200

    Fixup

--- .github/run_tests_on_input_files.sh
+++ .github/run_tests_on_input_files.sh
@@ -14,36 +14,36 @@
 # ======================================================================
 
 echo "Starting test jobs: $(date)"
-ls -lah .
 
 MODEL_CONFIG_FILE=$(realpath vbfml/config/convolutional_model.yml)
-ROOT_INPUT_DIR=$(realpath vbfml/.github/input_files)
+ROOT_INPUT_DIR=$(realpath .github/input_files)
 
 # Set up and run a training area
 cd vbfml/scripts
 
+mkdir -p output/test_job
 TRAINING_AREA=$(realpath output/test_job)
 
 echo "Setting up training area from ${ROOT_INPUT_DIR}"
-./train.py -d ${TRAINING_AREA} setup --input-dir ${ROOT_INPUT_DIR} --model-config ${MODEL_CONFIG_FILE}
+./train.py -d ${TRAINING_AREA} setup --input-dir ${ROOT_INPUT_DIR} --model-config ${MODEL_CONFIG_FILE} || exit 1;
 
 echo "Running training for a single epoch"
-./train.py -d ${TRAINING_AREA} train -n 1
+./train.py -d ${TRAINING_AREA} train -n 1 || exit 1;
 
 # Analyze training results
 echo "Analyzing training results"
-./analyze_training.py analyze ${TRAINING_AREA}
-./analyze_training.py plot ${TRAINING_AREA}
+./analyze_training.py analyze ${TRAINING_AREA} || exit 1;
+./analyze_training.py plot ${TRAINING_AREA} || exit 1;
 
 # Test pre-processing
 echo "Testing pre-processing of images"
-./preprocess_image.py rotate-all -i ${ROOT_INPUT_DIR}
+./preprocess_image.py rotate-all -i ${ROOT_INPUT_DIR} || exit 1;
 
 # Try plotting few pre-processed images
 PREPROCESSED_ROOT_DIR="${ROOT_INPUT_DIR}_preprocessed"
 NUM_EVENTS_TO_PLOT=5
 
 echo "Plotting ${NUM_EVENTS_TO_PLOT} images"
-./preprocess_image.py plot-rotation-all -i ${PREPROCESSED_ROOT_DIR} -n ${NUM_EVENTS_TO_PLOT}
+./preprocess_image.py plot-rotation-all -i ${PREPROCESSED_ROOT_DIR} -n ${NUM_EVENTS_TO_PLOT} || exit 1;
 
 echo "Finished test jobs: $(date)"
